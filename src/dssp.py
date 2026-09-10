9f1bf1d446f2933eafeb423976f29c81d91ef153
Selenay Kirtas <k.selenay93@gmail.com>
2026-09-10 13:40:49 +0200

    Mise à jour du projet

--- src/dssp.py
+++ src/dssp.py
@@ -1,15 +1,16 @@
-residus = {}    
 
-# 1 EXTRACTION DES COORDOONNÉES ATOMIQUES
+# --- 1 EXTRACTION DES COORDOONNÉES ATOMIQUES ---
 
-with open("thioredoxin.pdb", "r") as fichier:
+residus = {}
+
+with open("../data/thioredoxin.pdb", "r") as fichier:
     for ligne in fichier:
 
         if ligne.startswith("ATOM"):
 
             nom_atome = ligne[12:16].strip()
             nom_residue = ligne[17:20].strip()
-            nombre_residue = int(ligne[22:26])
+            numero_residue = int(ligne[22:26])
 
             x = float(ligne[30:38])
             y = float(ligne[38:46])
@@ -17,8 +18,8 @@ with open("thioredoxin.pdb", "r") as fichier:
 
             if nom_atome in ["N", "CA", "C", "O", "H"]:
 
-                if nombre_residue not in residus:
-                    residus[nombre_residue] = {
+                if numero_residue not in residus:
+                    residus[numero_residue] = {
                         "nom": nom_residue,
                         "N": None,
                         "CA": None,
@@ -27,39 +28,21 @@ with open("thioredoxin.pdb", "r") as fichier:
                         "H" : None
                     }
 
-                residus[nombre_residue][nom_atome] = (x, y, z) 
+                residus[numero_residue][nom_atome] = (x, y, z) 
 
-# print(residus)
+# print(residus)
 
 
-# 2 VÉRIFICATION PRÉSENCE DE NH + CALCULE DE dist_NH
+
+
+# --- 2 CALCULE DE DISTANCE + ÉNERGIE ---
 
 from math import sqrt
 
-def distance(atome1, atome2):
-	x1, y1, z1 = atome1
-	x2, y2, z2 = atome2
+def distance(coord_atome1, coord_atome2):
+	x1, y1, z1 = coord_atome1
+	x2, y2, z2 = coord_atome2
 	return sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
-
-
-#print("Vérification des distances N-H")
-
-for i in residus:
-    N = residus[i]["N"]
-    H = residus[i]["H"]
-
-    if N is not None and H is not None:
-        d_NH = distance(N, H)
-   #     print(
-    #        "Résidu", i,
-     #       residus[i]["nom"],
-      #      "N-H =", round(d_NH, 3), "Å"
-     #   )
-   # elif N is not None and H is None:
-   #     print("Abscense de NH -->", i, residus[i]["nom"])
-
-
-# 3 CALCULE DE DISTANCE + ÉNERGIE 
 
 q1 = 0.42
 q2 = 0.20
@@ -67,148 +50,250 @@ f = 332
 
 liaisons_H = []
 
-for i in residus:
-	for j in residus:
-		if (i != j 
-		and residus[j]["O"]
-		and residus[j]["C"]
-		and residus[j]["N"]
-		and residus[j]["H"] is not None):
+for residu1 in residus:
+	for residu2 in residus:
+		if (residu1 != residu2 and residus[residu1]["O"] is not None and residus[residu1]["C"] is not None and residus[residu2]["N"] is not None and residus[residu2]["H"] is not None):
 		
-			r_ON = distance(residus[i]["O"], residus[j]["N"]) # en A
-			r_CH = distance(residus[i]["C"], residus[j]["H"])
-			r_OH = distance(residus[i]["O"], residus[j]["H"])
-			r_CN = distance(residus[i]["C"], residus[j]["N"])
-			
-		#	print("Résidus :", i, j)
-		#	print("r_ON =", r_ON)
-		#	print("r_CH =", r_CH)
-		#	print("r_OH =", r_OH)
-		#	print("r_CN =", r_CN)
-
+			r_ON = distance(residus[residu1]["O"], residus[residu2]["N"]) 
+			r_CH = distance(residus[residu1]["C"], residus[residu2]["H"])
+			r_OH = distance(residus[residu1]["O"], residus[residu2]["H"])
+			r_CN = distance(residus[residu1]["C"], residus[residu2]["N"])
 
 			Energie = q1 * q2 * f * ((1 / r_ON) + (1 / r_CH) - (1 / r_OH) - (1 / r_CN))
 
-		#	print("Résidus :", i, j)
-		#	print("Énergie :", Energie)
+		#	print("Résidus :", residu1, residu2, "--> E =", Energie)
 
 			if Energie < -0.5:
-				liaisons_H.append((i, j))
+				liaisons_H.append((residu1, residu2))
+				
 
-#for i, j, energie in liaisons_H:
-#	print("Résidu", i, residus[i]["nom"], "-->", "Résidu", j, residus[j]["nom"],
-#        "| Énergie =", energie, "kcal/mol"
-   # )
-
-# Dans l'article ordre de grandeur attendu pour une liaison H idéale est : -3 kcal
+			#	print("Résidu", residu1, residus[residu1]["nom"], "-->", "Résidu", residu2, residus[residu2]["nom"], "| Énergie =", Energie, "kcal/mol")
 
 
-# 4 ASSIGNATION STRUCTURE SECONDAIRE
+print("Nombre total de liaisons hydrogènes :", len(liaisons_H))
 
-# 4-turn => difference de 4 entre résidu i et j 
+
+
+# --- 3 ASSIGNATION STRUCTURE SECONDAIRE ---
+
+# A) 4-turn - Hélice alpha
 
 turn4 = []
-for i, j in liaisons_H:
-	if j - i == 4:
-		turn4.append((i,j))
-		print("4-turn:", i, "-", j)
-		
-helices_alpha = []
-for i, j in turn4:	
-	if (i + 1, j + 1) in turn4:
-		helices_alpha.append((i, j))
-		print("Hélice alpha :", i, "-", j)
+for residu1, residu2 in liaisons_H:
+	if residu2 - residu1 == 4:
+		turn4.append((residu1,residu2))
+		print("4-turn:", residu1, "-", residu2)
+
+
+def trouver_helices(turn4):
+    helices = []
+
+    if len(turn4) == 0:
+        return helices
+
+    debut = turn4[0][0]
+    fin = turn4[0][1]
+    nombre_turns = 1
+
+    for numero_turn in range(1, len(turn4)):
+
+        if (turn4[numero_turn][0] == turn4[numero_turn - 1][0] + 1
+                and turn4[numero_turn][1] == turn4[numero_turn - 1][1] + 1):
+
+            fin = turn4[numero_turn][1]
+            nombre_turns += 1
+
+        else: 
+            if nombre_turns >= 2:
+                helices.append((debut, fin))
+
+            debut = turn4[numero_turn][0]
+            fin = turn4[numero_turn][1]
+            nombre_turns = 1
+
+    if nombre_turns >= 2:
+        helices.append((debut, fin))
+
+    return helices
+    
+    
+helices_alpha = trouver_helices(turn4)
+
+for debut, fin in helices_alpha:
+    print("Hélice alpha :", debut, "-", fin)
 
 
 
-# B-bridge parallèle (pont parallèle)
+
+# B) B-bridge parallèle (pont parallèle) / B-bridge antiparallèle (pont antiparallèle)
 
 bridge_paralleles = []
 
 for i in residus:
     for j in residus:
-        if i >= j:
-            continue
+        if i < j and abs(i - j) >= 3:  
+            if ((i - 1, j) in liaisons_H and (j, i + 1) in liaisons_H) \
+                    or ((j - 1, i) in liaisons_H and (i, j + 1) in liaisons_H):  
+                bridge_paralleles.append((i, j))
+                print("Pont parallèle :", i, "-", j)
 
-        if abs(i - j) < 3:
-            continue
+print(bridge_paralleles)
 
-        if ((i - 1, j) in liaisons_H and (j, i + 1) in liaisons_H) \
-        or ((j - 1, i) in liaisons_H and (i, j + 1) in liaisons_H):
-
-            bridge_paralleles.append((i, j))
-            print("Pont parallèle :", i, "-", j)
-
-# Ladders parallèle
-
-ladders_paralleles = []
-
-for i, j in bridge_paralleles:
-
-    # on cherche seulement le début d'une ladder
-    if (i - 1, j - 1) in bridge_paralleles:
-        continue
-
-    debut_i = i
-    debut_j = j
-
-    while (i + 1, j + 1) in bridge_paralleles:
-        i = i + 1
-        j = j + 1
-
-    if i > debut_i:
-        ladders_paralleles.append((debut_i, debut_j, i, j))
-
-
-for ladder in ladders_paralleles:
-    print("Ladder parallèle :", ladder)
-
-
-
-# B-bridge antiparallèle (pont antiparallèle)
 
 bridge_antiparalleles = []
 
 for i in residus:
     for j in residus:
-        if i >= j:
-            continue
+        if i < j and abs(i - j) >= 3:
+            if ((i, j) in liaisons_H and (j, i) in liaisons_H) \
+                    or ((i - 1, j + 1) in liaisons_H and (j - 1, i + 1) in liaisons_H):
+                bridge_antiparalleles.append((i, j))
+                print("Pont antiparallèle :", i, "-", j)
 
-        if abs(i - j) < 3:
-            continue
-
-        if ((i, j) in liaisons_H and (j, i) in liaisons_H) \
-        or ((i - 1, j + 1) in liaisons_H and (j - 1, i + 1) in liaisons_H):
-
-            bridge_antiparalleles.append((i, j))
-        #    print("Pont antiparallèle :", i, "-", j)
+print(bridge_antiparalleles)
 
 
-# Ladders antiparallèle
 
-ladders_antiparalleles = []
+# C) Ladders parallèle / Ladders antiparallèle
 
-for i, j in bridge_antiparalleles:
+ladders = {}
+nom_ladders_parallele = "a"
 
-    # on cherche seulement le début d'une ladder
-    if (i - 1, j + 1) in bridge_antiparalleles:
+for residu1, residu2 in bridge_paralleles:
+
+    if (residu1 - 1, residu2 - 1) in bridge_paralleles:
         continue
 
-    debut_i = i
-    debut_j = j
+    bridges = [(residu1, residu2)]
 
-    while (i + 1, j - 1) in bridge_antiparalleles:
-        i = i + 1
-        j = j - 1
+    while (residu1 + 1, residu2 + 1) in bridge_paralleles:
+        residu1 += 1
+        residu2 += 1
+        bridges.append((residu1,residu2))
+       
+    ladders[nom_ladders_parallele] = bridges
+    nom_ladders_parallele = chr(ord(nom_ladders_parallele) + 1)
+   
+   
+   
+nom_ladders_antiparallele = "A"
 
-    if i > debut_i:
-        ladders_antiparalleles.append((debut_i, debut_j, i, j))
+for residu1, residu2 in bridge_antiparalleles:
 
+    if (residu1 - 1, residu2 + 1) in bridge_antiparalleles:
+        continue
 
-for ladder in ladders_antiparalleles:
-    print("Ladder antiparallèle :", ladder)
+    bridges = [(residu1, residu2)]
+
+    while (residu1 + 1, residu2 - 1) in bridge_antiparalleles:
+        residu1 += 1
+        residu2 -= 1
+        bridges.append((residu1, residu2))
+
+    ladders[nom_ladders_antiparallele] = bridges
+    nom_ladders_antiparallele = chr(ord(nom_ladders_antiparallele) + 1)
     
-    
+for nom_ladder, bridges in ladders.items():
+	print("Ladder", nom_ladder, "-->", bridges)
+
+
+# print(ladders)
+
+
+# D) Sheets
+
+# 1e étape
+residus_communs = []
+noms = list(ladders)
+
+for i in range(len(noms)):
+    for j in range(i + 1, len(noms)):
+
+        ladder1 = noms[i]
+        ladder2 = noms[j]
+
+        residus1 = set()
+        residus2 = set()
+
+        for bridge in ladders[ladder1]:
+            residus1.add(bridge[0])
+            residus1.add(bridge[1])
+
+        for bridge in ladders[ladder2]:
+            residus2.add(bridge[0])
+            residus2.add(bridge[1])
+
+        if residus1 & residus2:
+        	residus_communs.append((ladder1,ladder2))
+        	print("Ladder", ladder1, "Ladder", ladder2, "-->", residus1 & residus2)
+
+# 2e étape
+Sheets = {}
+nom_sheet = "A"
+
+for ladder1, ladder2 in residus_communs:
+
+    connecte = False
+
+    for sheet in Sheets:
+
+        if ladder1 in Sheets[sheet] or ladder2 in Sheets[sheet]:
+
+            if ladder1 not in Sheets[sheet]:
+                Sheets[sheet].append(ladder1)
+
+            if ladder2 not in Sheets[sheet]:
+                Sheets[sheet].append(ladder2)
+
+            connecte = True
+
+    if not connecte:
+        Sheets[nom_sheet] = [ladder1, ladder2]
+        nom_sheet = chr(ord(nom_sheet) + 1)
+
+# print(Sheets)
+
+
+# --- 5 SUMMARY ---
+
+
+SUMMARY = {}
+for residu in residus:
+	SUMMARY[residu] = ""
+	
+for nom_ladder in ladders:
+	if len(ladders[nom_ladder]) == 1:
+		lettre = "B"
+	else:
+		lettre = "E"
+		
+	for bridge in ladders[nom_ladder]:
+		for residu in bridge:
+			SUMMARY[residu] = lettre
+	
+for debut, fin in helices_alpha:
+    for residu in range(debut, fin + 1):
+        SUMMARY[residu] = "H"
+
+
+print(SUMMARY)
+
+
+
+# --- 6 RÉSULTAT ---
+
+import pandas as pd
+from codes_acides_amines import codes_aa
+
+tableau = []
+
+for numero_residu in SUMMARY:
+	nom_residu = residus[numero_residu]["nom"]
+	code = codes_aa[nom_residu]
+	
+	tableau.append([numero_residu, code, SUMMARY[numero_residu]])
+
+df = pd.DataFrame(tableau, columns=["RESIDUE", "SEQUENCE" , "SUMMARY"])
 
 
 
